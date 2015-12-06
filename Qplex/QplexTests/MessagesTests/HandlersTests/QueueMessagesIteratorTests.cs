405aff7af2d50ea4ecead8581f812588454e6ffd
Eliran Koren <elirankoren@gmail.com>
2015-12-07 01:03:45 +0200

    Added QueueMessagesIterator tests

---
+++ Qplex/QplexTests/MessagesTests/HandlersTests/QueueMessagesIteratorTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Qplex.Messages;
+using Qplex.Messages.Handlers;
+
+namespace QplexTests.MessagesTests.HandlersTests
+{
+    [TestClass]
+    public class QueueMessagesIteratorTests
+    {
+        private IMessagesIterator _messagesIterator;
+        private Message _message;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _messagesIterator = new QueueMessagesIterator();
+            _message = new MockMessage();
+        }
+
+        #region HasNext
+
+        [TestMethod]
+        public void HasNextReturnsFalse()
+        {
+            Assert.IsFalse(_messagesIterator.HasNext());
+        }
+
+        #endregion
+
+        #region Integration
+
+        [TestMethod]
+        public void AddAddsToQueue()
+        {
+            _messagesIterator.Add(_message);
+            Assert.IsTrue(_messagesIterator.HasNext());
+        }
+
+        [TestMethod]
+        public void NextRemovesElementFromQueue()
+        {
+            _messagesIterator.Add(_message);
+            Assert.AreEqual(_message, _messagesIterator.Next());
+            Assert.IsFalse(_messagesIterator.HasNext());
+        }
+
+        [TestMethod]
+        public void AddAddsElementAfterNext()
+        {
+            _messagesIterator.Add(_message);
+            Assert.AreEqual(_message, _messagesIterator.Next());
+            _messagesIterator.Add(_message);
+            Assert.IsTrue(_messagesIterator.HasNext());
+        }
+
+        #endregion
+    }
+}
